2eb1c84e1b19e82f654f63280353c54089226b99
The Tuan Phan <thetuanphan@gmail.com>
2021-11-01 23:09:20 +0700

    Update_Template_Shop-Admin

---
+++ DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/CTHD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_CuaHangLaptop.Models
+{
+    public class CTHD
+    {
+    }
+}
---
+++ DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/HoaDon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_CuaHangLaptop.Models
+{
+    public class HoaDon
+    {
+        string maHD;
+        string maKH;
+        string maNV;
+        string maSK;
+        string ngayHD;
+        string diaChiGiaoHang;
+        long tongTien;
+        long thanhTien;
+
+        public HoaDon()
+        {
+        }
+
+        public HoaDon(string maHD, string maKH, string maNV, string maSK, string ngayHD, string diaChiGiaoHang, long tongTien, long thanhTien)
+        {
+            this.maHD = maHD;
+            this.maKH = maKH;
+            this.maNV = maNV;
+            this.maSK = maSK;
+            this.ngayHD = ngayHD;
+            this.diaChiGiaoHang = diaChiGiaoHang;
+            this.tongTien = tongTien;
+            this.thanhTien = thanhTien;
+        }
+
+        public string MaHD { get => maHD; set => maHD = value; }
+        public string MaKH { get => maKH; set => maKH = value; }
+        public int SoLuong { get => soLuong; set => soLuong = value; }
+        public string MauSac { get => mauSac; set => mauSac = value; }
+        public string CPU { get => cPU; set => cPU = value; }
+        public string RAM { get => rAM; set => rAM = value; }
+    }
+
+    
+}
