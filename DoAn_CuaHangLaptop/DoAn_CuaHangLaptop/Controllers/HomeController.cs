c44dd499e18d0c6c8b95e4fd1dbd64cd58ed8632
The Tuan Phan <thetuanphan@gmail.com>
2021-11-02 14:24:20 +0700

    Update_Stock-UI

--- DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Controllers/HomeController.cs
+++ DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Controllers/HomeController.cs
@@ -28,6 +28,31 @@ namespace DoAn_CuaHangLaptop.Controllers
             return View();
         }
 
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
