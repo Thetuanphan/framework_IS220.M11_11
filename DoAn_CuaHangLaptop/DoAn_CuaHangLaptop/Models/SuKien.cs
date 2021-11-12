f324254ce331733b99bee25532abe7acd2f8ba66
Ducvuive2 <82598608+Ducvuive2@users.noreply.github.com>
2021-11-13 00:11:13 +0700

    Tạo event khách hàng, sự kiện
    Tạo các event thêm, xóa , xóa , thông tin chi tiết, cập nhật của 2 model khách hàng và sự kiện

--- DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/SuKien.cs
+++ DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/SuKien.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace DoAn_CuaHangLaptop.Models
         DateTime ngayBD;
         DateTime ngayKT;
 
+        public SuKien() { }
         public SuKien(string maSK, string tenSK, int phanTramGiamGia, DateTime ngayBD, DateTime ngayKT)
         {
             this.maSK = maSK;
@@ -21,11 +23,18 @@ namespace DoAn_CuaHangLaptop.Models
             this.ngayBD = ngayBD;
             this.ngayKT = ngayKT;
         }
-
+        [Key]
+        [Display(Name = "Mã sự kiện")]
         public string MaSK { get => maSK; set => maSK = value; }
+        [Display(Name = "Tên sự kiện")]
         public string TenSK { get => tenSK; set => tenSK = value; }
+        [Display(Name = "Phần trăm giảm giá")]
         public int PhanTramGiamGia { get => phanTramGiamGia; set => phanTramGiamGia = value; }
+        [Display(Name = "Ngày bắt đầu sự kiện")]
+        //[DisplayFormat(DataFormatString = "{dd-MM-yyyy 0:00:00 }", ApplyFormatInEditMode = true)]
         public DateTime NgayBD { get => ngayBD; set => ngayBD = value; }
+        [Display(Name = "Ngày kết thúc sự kiện")]
+        //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime NgayKT { get => ngayKT; set => ngayKT = value; }
     }
 }
