faefaab77bcc9947e36cca88c5b1dc6ae338e2e4
The Tuan Phan <thetuanphan@gmail.com>
2021-11-13 12:32:53 +0700

    Revert "Merge branch 'Duc'"
    This reverts commit 9d46f1fdc50480deaf3e4a1825db749041628a7b, reversing
    changes made to 3a7670d4e430cc931b9aa02cbfb70409cb509a31.

--- DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/KhachHang.cs
+++ DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/KhachHang.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,36 +8,25 @@ namespace DoAn_CuaHangLaptop.Models
     public class KhachHang
     {
         string maKH;
-        string tenDN;
         string tenKH;
         string soDT;
         string email;
         string gioiTinh;
 
         public KhachHang() { }
-
-        public KhachHang(string maKH, string tenDN, string tenKH, string soDT, string email, string gioiTinh)
+        public KhachHang(string maKH, string tenKH, string soDT, string email, string gioiTinh)
         {
             this.MaKH = maKH;
-            this.TenDN = tenDN;
             this.TenKH = tenKH;
             this.SoDT = soDT;
             this.Email = email;
             this.GioiTinh = gioiTinh;
         }
-        [Key]
+
         public string MaKH { get => maKH; set => maKH = value; }
-        [Display(Name = "Tên Khách Hàng")]
-        [StringLength(20, ErrorMessage = "Tên khách hàng phải dưới 20 ký tự")]
         public string TenKH { get => tenKH; set => tenKH = value; }
-        public string TenDN { get => tenDN; set => tenDN = value; }
-        [Phone(ErrorMessage ="Mật khẩu không đúng yêu cầu")]
-        [Display(Name = "Số Điện thoại")]
-        [StringLength(10, ErrorMessage = "Số điện thoại phải dưới 10 ký tự")]
         public string SoDT { get => soDT; set => soDT = value; }
-        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
         public string Email { get => email; set => email = value; }
-
-
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
     }
 }
--- DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/SuKien.cs
+++ DoAn_CuaHangLaptop/DoAn_CuaHangLaptop/Models/SuKien.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,7 +13,6 @@ namespace DoAn_CuaHangLaptop.Models
         DateTime ngayBD;
         DateTime ngayKT;
 
-        public SuKien() { }
         public SuKien(string maSK, string tenSK, int phanTramGiamGia, DateTime ngayBD, DateTime ngayKT)
         {
             this.maSK = maSK;
@@ -23,18 +21,11 @@ namespace DoAn_CuaHangLaptop.Models
             this.ngayBD = ngayBD;
             this.ngayKT = ngayKT;
         }
-        [Key]
-        [Display(Name = "Mã sự kiện")]
+
         public string MaSK { get => maSK; set => maSK = value; }
-        [Display(Name = "Tên sự kiện")]
         public string TenSK { get => tenSK; set => tenSK = value; }
-        [Display(Name = "Phần trăm giảm giá")]
         public int PhanTramGiamGia { get => phanTramGiamGia; set => phanTramGiamGia = value; }
-        [Display(Name = "Ngày bắt đầu sự kiện")]
-        //[DisplayFormat(DataFormatString = "{dd-MM-yyyy 0:00:00 }", ApplyFormatInEditMode = true)]
         public DateTime NgayBD { get => ngayBD; set => ngayBD = value; }
-        [Display(Name = "Ngày kết thúc sự kiện")]
-        //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime NgayKT { get => ngayKT; set => ngayKT = value; }
     }
 }
